f7a3831cba03493e4e4f90bcc17581f25f922cb2
Muhammad Haggag <muhammad.haggag@lavaloon.com>
2024-02-14 16:48:19 +0200

    Clean up code (IDE hints/warnings)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,37 +14,37 @@ import org.apache.log4j.AppenderSkeleton
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
 import org.apache.log4j.spi.LoggingEvent
-import kotlin.reflect.typeOf
+import kotlin.system.exitProcess
 
 @Serializable
 data class Result(val msg: String, val errors: Array<String>)
 
 fun Ok(msg: String) {
-    System.out.println(Json.encodeToString(Result(msg, arrayOf())))
-    System.exit(0)
+    println(Json.encodeToString(Result(msg, arrayOf())))
+    exitProcess(0)
 }
 
 fun Err(msg: String, errors: Array<String> = arrayOf(), exitCode: Int = 1) {
-    System.out.println(Json.encodeToString(Result(msg, errors)))
-    System.exit(exitCode)
+    println(Json.encodeToString(Result(msg, errors)))
+    exitProcess(exitCode)
 }
 
-@Parameters(commandNames = arrayOf("csr"), commandDescription = "Generate a CSR for onboarding")
-class CsrCommand(val errorListener: ErrorListener) {
-    @Parameter(names = arrayOf("-o", "--output-csr"), required = true)
+@Parameters(commandNames = ["csr"], commandDescription = "Generate a CSR for onboarding")
+class CsrCommand(private val errorListener: ErrorListener) {
+    @Parameter(names = ["-o", "--output-csr"], required = true)
     lateinit var outputFile: String
 
-    @Parameter(names = arrayOf("-k", "--output-key"), required = true)
+    @Parameter(names = ["-k", "--output-key"], required = true)
     lateinit var outputKey: String
 
-    @Parameter(names = arrayOf("-c", "--config"), required = true)
+    @Parameter(names = ["-c", "--config"], required = true)
     lateinit var config: String
 
-    @Parameter(names = arrayOf("-p", "--pem"))
+    @Parameter(names = ["-p", "--pem"])
     var outputPem = false
 
     fun run() {
-        val props = ApplicationPropertyDto();
+        val props = ApplicationPropertyDto()
         props.isGenerateCsr = true
         props.csrFileName = outputFile
         props.privateKeyFileName = outputKey
@@ -60,10 +60,10 @@ class CsrCommand(val errorListener: ErrorListener) {
 }
 
 class Options {
-    @Parameter(names = arrayOf("-h", "--help"), description = "Display usage")
+    @Parameter(names = ["-h", "--help"], description = "Display usage")
     var help: Boolean? = null
 
-    @Parameter(names = arrayOf("-v", "--version"), description = "Display version")
+    @Parameter(names = ["-v", "--version"], description = "Display version")
     var version: Boolean? = null
 }
 
@@ -75,12 +75,12 @@ class ErrorListener : AppenderSkeleton() {
     }
 
     override fun requiresLayout(): Boolean {
-        return false;
+        return false
     }
 
     override fun append(event: LoggingEvent?) {
-        if (event == null) return;
-        if (event.getLevel().toInt() < Level.ERROR.toInt()) return;
+        if (event == null) return
+        if (event.getLevel().toInt() < Level.ERROR.toInt()) return
         errors.add(event.message.toString())
     }
 }
@@ -110,13 +110,13 @@ fun main(args: Array<String>) {
             Ok("Version: 1.0.0")
         }
 
-        when (commander.getParsedCommand()) {
+        when (commander.parsedCommand) {
             "csr" -> csr.run()
             else -> {
                 val sb = StringBuilder()
                 commander.usage(sb)
                 Err(sb.toString())
-            };
+            }
         }
 
         Err("Internal error")
