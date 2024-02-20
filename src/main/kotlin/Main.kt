685d761bcc4eaaf62401feb0ee267b8a9c353f3c
Muhammad Haggag <muhammad.haggag@lavaloon.com>
2024-02-20 18:44:45 +0200

    Add hashing, signing and QR code generation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,13 @@ import com.beust.jcommander.JCommander
 import com.beust.jcommander.Parameter
 import com.beust.jcommander.ParameterException
 import com.beust.jcommander.Parameters
+import com.gazt.einvoicing.signing.service.model.InvoiceSigningResult
+import com.starkbank.ellipticcurve.utils.File
+import com.zatca.config.ResourcesPaths
+import com.zatca.config.ResourcesPaths.Paths
 import com.zatca.sdk.dto.ApplicationPropertyDto
 import com.zatca.sdk.service.CsrGenerationService
+import com.zatca.sdk.service.InvoiceSigningService
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -14,18 +19,28 @@ import org.apache.log4j.AppenderSkeleton
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
 import org.apache.log4j.spi.LoggingEvent
+import java.io.FileOutputStream
+import java.util.*
 import kotlin.system.exitProcess
 
 @Serializable
-data class Result(val msg: String, val errors: Array<String>)
+data class Result<TData>(val msg: String, val errors: Array<String>, val data: TData?)
+
+@Serializable
+data class SigningData(val hash: String, val qrCode: String)
+
+inline fun <reified TData> Ok(msg: String, data: TData) {
+    println(Json.encodeToString(Result(msg, arrayOf(), data)))
+    exitProcess(0)
+}
 
 fun Ok(msg: String) {
-    println(Json.encodeToString(Result(msg, arrayOf())))
+    println(Json.encodeToString(Result(msg, arrayOf(), null)))
     exitProcess(0)
 }
 
 fun Err(msg: String, errors: Array<String> = arrayOf(), exitCode: Int = 1) {
-    println(Json.encodeToString(Result(msg, errors)))
+    println(Json.encodeToString(Result(msg, errors, null)))
     exitProcess(exitCode)
 }
 
@@ -56,6 +71,58 @@ class CsrCommand(private val errorListener: ErrorListener) {
             Err("Failed to generate CSR", errorListener.errors.toTypedArray())
 
         return Ok("CSR generated successfully")
+    }
+}
+
+@Parameters(commandNames = ["sign"], commandDescription = "Generates a signed invoice from a source invoice")
+class SignCommand(private val errorListener: ErrorListener) {
+    @Parameter(names = ["-o", "--output"], required = true, description = "Output XML file (signed invoice)")
+    lateinit var outputFile: String
+
+    @Parameter(names = ["-c", "--cert"], required = true, description = "Signing certificate path")
+    lateinit var certFile: String
+
+    @Parameter(names = ["-k", "--private-key"], required = true, description = "Private key path")
+    lateinit var privateKeyPath: String
+
+    @Parameter(required = true, description = "Input XML file (invoice)")
+    lateinit var inputFile: String
+
+
+    fun run() {
+        val props = ApplicationPropertyDto()
+        props.isGenerateSignature = true
+        props.isGenerateQr = true
+        props.isGenerateHash = true
+        props.invoiceFileName = inputFile
+        props.outputInvoiceFileName = outputFile
+
+        val path = kotlin.io.path.createTempFile(prefix = "cert-base64")
+        FileOutputStream(path.toFile()).use {
+            val certBytes = File.readBytes(certFile)
+            val encoded = Base64.getEncoder().encode(certBytes)
+            it.write(encoded)
+        }
+
+        val loader = ::InvoiceSigningService.javaClass.classLoader
+        val paths = Paths()
+        paths.xsdPth = loader.getResource("xsds/UBL2.1/xsd/maindoc/UBL-Invoice-2.1.xsd")!!.toString()
+        paths.enSchematronPath = loader.getResource("schematrons/CEN-EN16931-UBL.xsl")!!.toString()
+        paths.certificatePath = path.toString()
+        paths.privateKeyPath = privateKeyPath
+        ResourcesPaths.setPaths(paths)
+
+        val signingService = InvoiceSigningService()
+        if (!signingService.generate(props))
+            Err("Failed to sign invoice", errorListener.errors.toTypedArray())
+
+        // The signing service does not expose the result directly, and we don't want to rely on parsing text output
+        // to extract the hash. The QR code is never output either, and we don't want to have to load the XML to
+        // extract it
+        val field = signingService::class.java.getDeclaredField("invoiceSigningResult")
+        field.isAccessible = true
+        val result = field.get(signingService) as InvoiceSigningResult
+        return Ok("Invoice signed successfully", SigningData(result.invoiceHash, result.qrCode))
     }
 }
 
@@ -92,11 +159,13 @@ fun main(args: Array<String>) {
 
         val options = Options()
         val csr = CsrCommand(errorListener)
+        val sign = SignCommand(errorListener)
 
         val commander = JCommander.newBuilder()
             .programName("lava-zatca")
             .addObject(options)
             .addCommand(csr)
+            .addCommand(sign)
             .build()
 
         commander.parse(*args)
@@ -107,11 +176,12 @@ fun main(args: Array<String>) {
         }
 
         if (options.version == true) {
-            Ok("Version: 1.0.0")
+            Ok("Version: 1.1.0")
         }
 
         when (commander.parsedCommand) {
             "csr" -> csr.run()
+            "sign" -> sign.run()
             else -> {
                 val sb = StringBuilder()
                 commander.usage(sb)
